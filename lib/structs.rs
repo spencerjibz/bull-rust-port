d4ccb07991072c4d0a055d03927a40c6d9cabc59
Spencerjibz <=spencernajib2@gmail.com>
2023-05-28 23:09:09 +0100

    added implementation of build from str, build from map

--- lib/structs.rs
+++ lib/structs.rs
@@ -3,9 +3,11 @@
 // properties.
 pub use derive_redis_json::RedisJsonValue;
 use rand::prelude::*;
-pub use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, default, fmt::Display};
 use redis::{FromRedisValue, RedisError, RedisResult, ToRedisArgs, Value};
+pub use serde::{Deserialize, Serialize};
+use std::{borrow::Borrow, collections::HashMap, default, fmt::Display};
+
+use crate::to_static_str;
 
 #[derive(Debug, Default, Deserialize, Serialize, RedisJsonValue, Clone, Copy)]
 pub struct KeepJobs {
@@ -100,26 +102,83 @@ impl Default for WorkerOptions {
     }
 }
 
+#[derive(Debug, Default)]
 pub struct QueueOptions<'d> {
     pub prefix: Option<&'d str>,
 }
 
-#[derive(Debug, Clone,Deserialize, Serialize,RedisJsonValue,)]
+#[derive(Debug, Clone, Default, Deserialize, Serialize)]
 // make all fields public
 pub struct JobJsonRaw {
-    pub id: String,
-    pub name: String,
-    pub data: String,
-    pub delay: String,
-    pub opts: String,
-    pub progress: String,
-    pub attempts_made: String,
-    pub timestamp: String,
-    pub failed_reason: String,
-    pub stack_trace: Vec<String>,
-    pub return_value: String,
-    pub parent: Option<String>,
-    pub rjk: Option<String>,
-    pub finished_on: Option<String>,
-    pub processed_on: Option<String>,
+    #[serde(borrow)]
+    pub id: &'static str,
+    #[serde(borrow)]
+    pub name: &'static str,
+    #[serde(borrow)]
+    pub data: &'static str,
+    #[serde(borrow)]
+    pub delay: &'static str,
+    #[serde(borrow)]
+    pub opts: &'static str,
+    #[serde(borrow)]
+    pub progress: &'static str,
+    #[serde(borrow)]
+    pub attempts_made: &'static str,
+    #[serde(borrow)]
+    pub timestamp: &'static str,
+    #[serde(borrow)]
+    pub failed_reason: &'static str,
+    #[serde(borrow)]
+    pub stack_trace: Vec<&'static str>,
+    #[serde(borrow)]
+    pub return_value: &'static str,
+    #[serde(borrow)]
+    pub parent: Option<&'static str>,
+    #[serde(borrow)]
+    pub rjk: Option<&'static str>,
+    #[serde(borrow)]
+    pub finished_on: Option<&'static str>,
+    #[serde(borrow)]
+    pub processed_on: Option<&'static str>,
+}
+
+impl JobJsonRaw {
+    pub fn from_map(map: HashMap<String, String>) -> anyhow::Result<JobJsonRaw> {
+        let mut job = JobJsonRaw::default();
+        for (k, v) in map {
+            let v = to_static_str(v);
+            match k.as_str() {
+                "id" => job.id = v,
+                "name" => job.name = v,
+                "data" => job.data = v,
+                "delay" => job.delay = v,
+                "opts" => job.opts = v,
+                "progress" => job.progress = v,
+                "attempts_made" => job.attempts_made = v,
+                "timestamp" => job.timestamp = v,
+                "failed_reason" => job.failed_reason = v,
+                "stack_trace" => job.stack_trace = serde_json::from_str(v)?,
+                "return_value" => job.return_value = v,
+                "parent" => job.parent = Some(v),
+                "rjk" => job.rjk = Some(v),
+                "finished_on" => job.finished_on = Some(v),
+                "processed_on" => job.processed_on = Some(v),
+                _ => (),
+            }
+        }
+        Ok(job)
+    }
+    #[allow(non_snake_case)]
+    pub fn fromStr(s: &'static str) -> anyhow::Result<JobJsonRaw> {
+        // passed the map;
+        let map: HashMap<String, String> = serde_json::from_str(s)?;
+        let json = JobJsonRaw::from_map(map)?;
+        Ok(json)
+    }
+    pub fn save_to_file(&self, path: &str) -> anyhow::Result<()> {
+        let mut file = std::fs::File::create(path)?;
+        serde_json::to_writer_pretty(file, self)?;
+
+        Ok(())
+    }
 }
