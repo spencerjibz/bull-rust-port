b4bcc3df139f246a66a41d21667c1ff0b5221851
Spencerjibz <=spencernajib2@gmail.com>
2023-06-22 18:31:39 +0100

    Update dependencies and format timestamps

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,6 @@ use std::time::{Instant, SystemTime};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // print backtrace if either Ru
-    use std::backtrace::Backtrace;
-    // or forcibly capture the backtrace regardless of environment variable configuration
-
     let n = Instant::now();
     let pass = dotenv!("REDIS_PASSWORD");
 
@@ -19,36 +15,50 @@ async fn main() -> anyhow::Result<()> {
     config.insert("password", pass);
     let redis_opts = RedisOpts::Config(config);
     let mut client = RedisConnection::init(redis_opts.clone()).await?;
-    // check the connection
+
     let keeps_opts = KeepJobs {
         age: None,
         count: Some(-1),
     };
 
     let mut con = client.conn;
-    // set time and
+
     let mut worker = WorkerOptions::default();
     let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
     con.set("worker", worker).await?;
     // get the values and log it;
     let value: WorkerOptions = con.get("worker").await?;
 
-    let result: HashMap<String, String> = con.hgetall("bull:pinningQueue:204").await?;
+    let result: HashMap<String, String> = con.hgetall("bull:pinningQueue:167").await?;
 
     if result.is_empty() {
         println!("No data found");
     } else {
         println!("Data found");
 
-        //json.save_to_file("test.json")?;
         let mut queue = Queue::<'_>::new("test", redis_opts, QueueOptions::default()).await?;
-        // println!("{:#?}", worker.clone());
-        // let j = serde_json::to_string(&result).unwrap_or("{}".to_string());
+
         let contents = serde_json::to_string(&result).unwrap_or("{}".to_string());
 
-        let job = Job::<Data, Option<ReturnedData>>::from_json(&queue, contents, "167").await?;
+        let job = Job::<Data, ReturnedData>::from_json(&queue, contents, "167").await?;
         println!("{:#?}", job);
-        // println!(" {value:#?}");
+
+        use chrono::{DateTime, NaiveDate, NaiveDateTime, Utc};
+
+        let (finished_on, processed_on, date) = (job.finished_on, job.processed_on, job.timestamp);
+
+        let finished_on = NaiveDateTime::from_timestamp_millis(finished_on).unwrap();
+        let processed_on = NaiveDateTime::from_timestamp_millis(processed_on).unwrap();
+        let date = NaiveDateTime::from_timestamp_millis(date);
+
+        println!("finished_on: {:#?}", finished_on);
+        println!("processed_on: {:#?}", processed_on);
+        let datetime: DateTime<Utc> = DateTime::from_utc(date.unwrap(), Utc);
+
+        // Format the datetime how you want
+        let newdate = datetime.format("%Y-%m-%d %H:%M:%S");
+        println!("date: {}", newdate);
+
         println!("{:?}", n.elapsed());
     }
     Ok(())
