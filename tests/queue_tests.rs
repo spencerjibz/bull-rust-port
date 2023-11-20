5958ce7357fa688f0ef26b461478fa57191f8278
Spencerjibz <=spencernajib2@gmail.com>
2023-11-20 07:21:45 +0000

    Add `remove_job_from_queue` test and clean up job after use

--- tests/queue_tests.rs
+++ tests/queue_tests.rs
@@ -40,8 +40,35 @@ mod tests {
 
         let job: Job<'_, Data, String> = queue.add("test", data, job_opts).await?;
 
-        assert_eq!(job.id, id);
-
+        assert_eq!(job.id, id.clone());
+        // cleanup
+        queue.remove_job(id, false).await?;
         Ok(())
     }
+
+    #[tokio::test]
+
+    async fn remove_job_from_queue() -> anyhow::Result<()> {
+        let queue = QUEUE.force().await;
+
+        let data = Data {
+            socket_id: "w3ess2".to_ascii_lowercase(),
+            cid: "Qufaufsduafsudafusaufusdaf".to_ascii_lowercase(),
+            file_id: "".to_owned(),
+            sizes: vec![],
+            user_id: "123".to_owned(),
+            tracking_id: "fadfasfdsaf".to_ascii_lowercase(),
+        };
+        let job_opts = JobOptions::default();
+        let id = job_opts.job_id.clone().unwrap();
+
+        let job: Job<'_, Data, String> = queue.add("test", data, job_opts).await?;
+
+        queue.remove_job(id.clone(), false).await?;
+        let result: Option<Job<'_, Data, String>> = Job::from_id(queue, &job.id).await?;
+
+        assert_eq!(result, None);
+        Ok(())
+    }
+    
 }
