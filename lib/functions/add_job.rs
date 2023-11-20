0cee8520e678d9ae0d670ae704d024d4d34063c0
Spencerjibz <=spencernajib2@gmail.com>
2023-11-20 08:39:52 +0000

    Update get_next_delayed_timestamp to use pop instead of indexing

--- lib/functions/add_job.rs
+++ lib/functions/add_job.rs
@@ -12,12 +12,12 @@ async fn get_next_delayed_timestamp(
     delayed_key: String,
     con: &mut Connection,
 ) -> anyhow::Result<Option<i64>> {
-    let result: Vec<String> = Cmd::zrange_withscores(delayed_key, 0, 0)
+    let mut result: Vec<String> = Cmd::zrange_withscores(delayed_key, 0, 0)
         .query_async(con)
         .await?;
 
     if !result.is_empty() {
-        if let Ok(mut next_time_stamp) = result[2].parse::<i64>() {
+        if let Ok(mut next_time_stamp) = result.pop().unwrap().parse::<i64>() {
             next_time_stamp /= 0x1000;
 
             return Ok(Some(next_time_stamp));
