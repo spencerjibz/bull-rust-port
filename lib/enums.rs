a803962265e910582b19600da927a93feec2962c
Spencer Najib <spencernajib2@gmail.com>
2024-09-02 21:47:58 +0100

    add better error types

--- lib/enums.rs
+++ lib/enums.rs
@@ -1,14 +1,22 @@
 use derive_redis_json::RedisJsonValue;
 use redis::FromRedisValue;
 use serde::{Deserialize, Serialize};
+
+use thiserror::Error;
 #[repr(i8)]
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Deserialize, Serialize, RedisJsonValue)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Deserialize, Serialize, RedisJsonValue, Error)]
 pub enum ErrorCode {
+    #[error("The job does not exist")]
     JobNotExist = -1,
+    #[error("The job lock does not exist")]
     JobLockNotExist = -2,
+    #[error("The job is not in the expected state")]
     JobNotInState = -3,
+    #[error("The job has pending dependencies")]
     JobPendingDependencies = -4,
+    #[error("The parent job does not exist")]
     ParentJobNotExist = -5,
+    #[error("The job lock does not match")]
     JobLockMismatch = -6,
 }
 
@@ -39,4 +47,22 @@ impl TryFrom<i8> for ErrorCode {
             _ => Err(()),
         }
     }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize, RedisJsonValue, Error)]
+pub enum PttlError {
+    #[error("No expiration associated with Key({0})")]
+    NoExpirationWithKey(String),
+    #[error("Key not Found")]
+    KeyNotFound,
+}
+
+#[derive(Debug, Error)]
+pub enum QueueError {
+    #[error("RedisError: {0}")]
+    RedisError(#[from] redis::RedisError),
+    #[error("PoolError: {0}")]
+    DealPoolError(#[from] deadpool_redis::PoolError),
+    #[error("RedisPttlError: {0}")]
+    RedisPttLError(PttlError),
 }
